ff536590437896723329378fecc07c8a1dfd36ae
Manuel Fombuena <mfombuena@innovara.co.uk>
2023-02-28 11:49:09 +0100

    Autoreply check to avoid loops. Binary reinjection
    Added check_autoreply() to detect common autoreply and automated headers, in order to avoid loops and responding to newsletters and mailing lists.
    
    Added In-Reply-To header to ensure autoreply emails are threaded properly by the recipient
    
    Changed how the email is reinjected to binary to avoid unnecessary tampering and related encoding issues

--- autoreply.py
+++ autoreply.py
@@ -5,7 +5,7 @@ import sys
 import json
 import os.path
 from os import chmod
-from email import message_from_file
+from email import message_from_bytes
 from email.message import EmailMessage
 from email.utils import make_msgid
 from subprocess import Popen, PIPE
@@ -15,7 +15,7 @@ from datetime import datetime
 def log(message):
   '''Logs messages to ~/autoreply.log.'''
   if logging == True:
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     log_file = os.path.expanduser('~') + '/autoreply.log'
     with open(log_file, 'a+', encoding='utf-8') as log:
       log.write(now + ': ' + message + '\n')
@@ -56,11 +56,11 @@ def open_json():
   # If the file .json doesn't exist, it creates a template
   except FileNotFoundError:
     create_json()
-    sys.exit("Couldn't find ~/autoreply.json. New template created.")
+    sys.exit('couldn\'t find ~/autoreply.json. New template created.')
   return data
 
 
-def generate_email(sender, recipient, replyto, subject, body, attachment_path=None, test=False):
+def generate_email(sender, recipient, original_id, replyto, subject, body, attachment_path=None, test=False):
   '''Creates an email message object with an attachement (optional).'''
   # TODO: HTML body with formatting instead of plain text?
   message = EmailMessage()
@@ -71,8 +71,11 @@ def generate_email(sender, recipient, replyto, subject, body, attachment_path=No
   message['Message-ID'] = make_msgid()
   message['Reply-to'] = replyto
   if test == False:
-    message['X-Autoreply'] = "yes"
-    message['Auto-Submitted'] = "auto-replied"
+    message['In-Reply-To'] = original_id
+    message['Auto-Submitted'] = 'auto-replied'
+    message['X-Autoreply'] = 'yes'
+    message['X-Auto-Response-Suppress'] = 'All'
+    message['Precedence'] = 'auto_reply'
   message.set_content(body)
   # Process the attachment and add it to the email
   if attachment_path != None:
@@ -99,18 +102,52 @@ def send_email(message):
   mail_server.quit()
 
 
-def reinject_email(message, sender, recipients):
+def reinject_email(message, sender, recipients, original_id):
   '''Sends original email back to Postfix for final delivery.'''
   # NOTE: According to Postfix's FILTER documentation
   # you must not use -t to re-inject the message
   separator = ','
   r_recipients = separator.join(recipients)
-  log('re-injecting email to: ' + r_recipients)
-  process = Popen(["/usr/sbin/sendmail", "-f", sender, "-G", "-oi", r_recipients], stdin=PIPE)
-  process.communicate(message.as_bytes())
+  log('re-injecting ' + str(original_id))
+  log('recipients: ' + r_recipients)
+  process = Popen(['/usr/sbin/sendmail', '-f', sender, '-G', '-oi', r_recipients], stdin=PIPE)
+  process.communicate(message)
 
 
-def autoreply(sender, recipients):
+def check_autoreply(message, original_id):
+  '''Checks if an incoming email is an autoreply itself to avoid loops'''
+  '''For more information please see https://www.arp242.net/autoreply.html'''
+  # Defined in RFC 3834. ‘Official’ standard to indicate a message is an autoreply
+  log('checking autoreply or automated headers on ' + str(original_id))
+  if message['Auto-submitted'] != 'no':
+    log('Auto-submitted present, not sending autoreply')
+    return True
+  # Other non RFC-compliant Auto-submitted headers
+  elif message['X-Autoreply'] != None or message['X-Autorespond'] != None:
+    log('X-Autoreply or X-Autorespond present, not sending autoreply')
+    return True
+  # Defined by Microsoft. Header used by Microsoft Exchange, Outlook, and perhaps others
+  elif message['X-Auto-Response-Suppress'] in ('DR', 'AutoReply', 'All'):
+    log('X-Auto-Response-Suppress is DR, AutoReply or All, not sending autoreply')
+    return True
+  # Defined in RFC 2919. Most of the time you don’t want to send autoreplies to mailing lists or newsletters
+  elif message['List-Id'] != None or message['List-Unsubscribe'] != None:
+    log('List-Id or List-Unsubscribe present, not sending autoreply')
+    return True
+  # Defined by Google. Gmail uses this header to identify newsletters and uses it to generate statistics
+  elif message['Feedback-ID'] != None:
+    log('Feedback-ID present, not sending autoreply')
+    return True
+  # Mentioned in RFC 2076 where its use is discouraged, but this header is commonly encountered
+  elif str(message['Precedence']).lower() in ('bulk', 'auto_reply', 'list'):
+    log('Precedence is bulk, auto_reply or list, not sending autoreply')
+    return True
+  else:
+    log('no autoreply or automated headers found on ' + str(original_id))
+    return False
+
+
+def autoreply(sender, recipients, original_id):
   '''Sends auto-reply email from recipient to sender when the recipient is in ~/autoreply.json.'''
   settings = open_json()
   # Iterates through JSON autoreply objects
@@ -119,6 +156,7 @@ def autoreply(sender, recipients):
     if recipient['email'] in recipients:
       log('autoreply triggered')
       log('sender is ' + str(sender))
+      log('Message-Id is ' + str(original_id))
       log('recipients are ' + str(recipients))
       log('recipient that triggered the script is ' + str(recipient['email']))
       # Checks if the auto-reply To and From are different to avoid an infinite loop
@@ -127,6 +165,7 @@ def autoreply(sender, recipients):
         message = generate_email(
           recipient['email'],
           sender,
+          original_id,
           recipient['reply-to'],
           recipient['subject'],
           recipient['body']
@@ -178,19 +217,25 @@ def main():
     logging = True
   else:
     logging = False
+  log('autoreply.py has been invoked')
   # Sender and recipients of the source email sent by Postfix as ./autoreply.py ${sender} ${recipient}
   # see README.md
   sender = sys.argv[1]
   # ${recipient} expands to as many recipients as the message contains
   recipients = sys.argv[2:]
   # Original message from STDIN
-  original_msg = message_from_file(sys.stdin)
+  binary_msg = sys.stdin.buffer.read()
+  # Message object
+  original_msg = message_from_bytes(binary_msg)
+  original_id = original_msg['Message-ID']
   # Re-injects original email into Postfix.
-  # If the purpose of the script was to do something with the original email this should be done later
-  reinject_email(original_msg, sender, recipients)
-  # Sends the auto-reply
-  autoreply(sender, recipients)
+  # If the purpose of the script was to do something else with the original email, re-injecting should be done later
+  reinject_email(binary_msg, sender, recipients, original_id)
+  auto_submitted = check_autoreply(original_msg, original_id)
+  if auto_submitted == False:
+    # Sends the auto-reply
+    autoreply(sender, recipients, original_id)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
    main()
